2f0cbfe090c0b85dd25a80e8300f8a84e63ec35a
Stephen Bradshaw <root@Elliss-MacBook-Pro.local>
2024-05-19 23:06:54 -0500

    update with just hello world, per final assignment

--- hello.js
+++ hello.js
@@ -5,27 +5,6 @@ to the console (the panel on the right)
 */
 
 console.log("Hello, World!");
-console.log(5+7);
-var cake = 7;
-var bake = 9;
-console.log(cake+bake);
-var msg1 = "this is the first message";
-var msg2 = "this is the second message";
-
-console.log(msg1 + " " + msg2);
-console.log('"It is not death a man should fear, but he should fear never beginning to live." -Marcus Aurelius')
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question("What is your name? ", function (answer) {
-   console.log(`Hello, ${answer}`);
-   rl.close();
- });
-
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
